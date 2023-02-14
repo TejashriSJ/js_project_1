3833f28f6766aa3ae907d4da80f6c00318943a6c
TejashriSJ <tejashri.j.23.2@mountblue.tech>
2023-02-14 16:57:52 +0530

    Add problem2 file with problem2 method
    This will provide the details of the last car in the inventory

---
+++ problem2.cjs
@@ -0,0 +1,7 @@
+
+function problem2(inventory){ 
+    
+    return 'Last car is a ' + inventory[inventory.length-1].car_make +' ' + inventory[inventory.length-1].car_model;
+}
+
+module.exports = problem2;
