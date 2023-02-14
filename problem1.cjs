6c1e65e28cca27ead187b79db679566186e2eeb7
TejashriSJ <tejashri.j.23.2@mountblue.tech>
2023-02-14 13:47:50 +0530

    add problem1.cjs and testproblem1.cjs files with problem1 method

---
+++ problem1.cjs
@@ -0,0 +1,13 @@
+function problem1(inventory){
+
+for(let carIndex =0; carIndex< inventory.length ; carIndex++){
+     
+    if (33 === inventory[carIndex].id){
+        return 'Car 33 is a '+ inventory[carIndex].car_year + ' ' +inventory[carIndex].car_make +' '+ inventory[carIndex].car_model;
+    }
+
+}
+
+}
+
+module.exports = problem1 
