7cf7459cce806cc4a6626677abd8968b38f93a5d
TejashriSJ <tejashri.j.23.2@mountblue.tech>
2023-02-16 16:15:28 +0530

    Update problem1 file
    Bug related to searchID 0 is cleared

--- problem1.cjs
+++ problem1.cjs
@@ -1,18 +1,15 @@
 function problem1(inventory , searchId){
 
-
-if (! inventory || inventory.length === 0 || ! searchId || !(Array.isArray(inventory)) || !(Number.isInteger(searchId)) ){
-    return []
+if (! inventory || inventory.length === 0 || !(Number.isInteger(searchId)) || !(Array.isArray(inventory)) || !(Number.isInteger(searchId)) ){
+    return [];
 }
 else{
     for(let carIndex =0; carIndex< inventory.length ; carIndex++){
-     
+        
         if (searchId === inventory[carIndex].id){
-            console.log( 'Car ' + searchId + ' is a '+ inventory[carIndex]["car_year"] + ' ' +  inventory[carIndex]["car_make"] + ' ' + inventory[carIndex]["car_model"] );
-            return inventory[carIndex]
-            
+            console.log("Car " + carIndex +" is a " + inventory[carIndex].car_year + ' ' + inventory[carIndex].car_make + ' ' + inventory[carIndex].car_model)
+            return inventory[carIndex];    
         }
-    
     }
 }
 
